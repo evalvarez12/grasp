10abbc1357ce985d8ad6b6d50d41ba228bd33931
Eduardo Villaseñor <evalvarez12@gmail.com>
2016-01-31 21:20:28 -0600

    Fisrt webpage testing

---
+++ src/site.py
@@ -0,0 +1,69 @@
+import web
+import string
+import bagwords.extract as ext
+
+
+
+
+render = web.template.render('web/templates/')
+
+db = web.database(dbn='sqlite', db='source.db')
+
+# DEF_TEMPLATE =  string.Template(u'<span class="hover">$word<span class="appear">$def</span></span>')
+
+urls = (
+    '/', 'index',
+    '/solve', 'solve',
+    '/clear', 'clear'
+
+
+)
+
+class index():
+    def GET(self):
+        return render.index('')
+
+
+class clear:
+    def GET(self):
+        raise web.seeother('/')
+
+
+
+class solve :
+    def POST(self) :
+        winput = web.input()
+        text = web_format(winput.text)
+        definitions = {}
+        for i in text :
+            for j in i :
+                word = j.lower()
+                # s = "SELECT times from bag where word = '" + word + "'"
+                # count = db.query(s)
+                s = 'word = "{}"'.format(word)
+                count = db.select('bag',what='times',where=s)
+                if count :
+                    if count[0] < 1500 :
+                        # s = "SELECT def from dic where word = '" + i + "'"
+                        # fetch = db.query(s)
+                        fetch = db.select('dic',what='def',where=s)
+                        if fetch :
+                            word_def = unicode(fetch[0],'utf-8')
+                            defintions[i]=word_def
+
+        return render.show(text,definitions)
+
+        # raise web.seeoter('/show')
+
+
+def web_format(content) :
+    lines = content.split('\n')
+    result = []
+    for i in lines :
+        result.append(ext.process_line(i))
+    return result
+
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
--- src/web/page.py
+++ src/web/page.py
@@ -2,25 +2,32 @@ import web
 
 render = web.template.render('templates/')
 
-db = web.database(dbn='sqlite', db='test')
+# db = web.database(dbn='sqlite', db='test')
 
 urls = (
     '/', 'index',
-    '/add', 'add'
+    '/show', 'show',
+    '/clear','clear'
 )
 
-class index:
+class index():
     def GET(self):
-        todos = db.select('todo')
-        return render.index(todos)
+        return render.index('')
 
 
-class add:
+class show:
     def POST(self):
         i = web.input()
-        n = db.insert('todo', title=i.title)
+        lines = i.text.split('\n')
+        # if 'as' in text :
+        #     # text = text.replace('as','<span class="hover">as<span class="appear"> <h1>My popup</h1>Hitherto and whenceforth.</span></span>')
+        # print "ESTO ES T: ", repr(text)
+        return render.show(lines)
+
+class clear:
+    def GET(self):
         raise web.seeother('/')
-        
+
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
