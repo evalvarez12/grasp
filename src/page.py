ecab7562806c114ee7b316a0d8907477db84ead0
Eduardo <evalvarez12@gmail.com>
2016-02-01 22:58:12 -0600

    Ugly web working

--- src/page.py
+++ src/page.py
@@ -5,7 +5,7 @@ import bagwords.extract as ext
 
 
 
-render = web.template.render('web/templates/')
+render = web.template.render('templates/')
 
 db = web.database(dbn='sqlite', db='source.db')
 
@@ -42,20 +42,26 @@ class solve :
                 # count = db.query(s)
                 s = 'word = "{}"'.format(word)
                 count = db.select('bag',what='times',where=s)
+                print "COUNT:",count
+                count = count.list()
                 if count :
-                    if count[0] < 1500 :
+                    if count[0]['times'] < 1500 :
                         # s = "SELECT def from dic where word = '" + i + "'"
                         # fetch = db.query(s)
                         fetch = db.select('dic',what='def',where=s)
+                        fetch = fetch.list()
                         if fetch :
-                            word_def = unicode(fetch[0],'utf-8')
-                            defintions[i]=word_def
+                            # word_def = unicode(fetch[0]['def'],'utf-8')
+                            word_def = fetch[0]['def']
+                            definitions[j] = word_def
                         else :
                             s = 'word = "{}"'.format(j)
                             fetch = db.select('dic',what='def',where=s)
+                            fetch = fetch.list()
                             if fetch :
-                                word_def = unicode(fetch[0],'utf-8')
-                                defintions[i]=word_def
+                                word_def = fetch[0]['def']
+                                # word_def = unicode(fetch[0]['def'],'utf-8')
+                                definitions[j] = word_def
 
         return render.show(text,definitions)
 
